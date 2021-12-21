498582b92aafad303a0b316da4135c51341df25e
NikushaTomaradze <nikatomaradze15@gmail.com>
2021-12-21 13:55:28 +0400

    Initial commit

---
+++ app/src/main/java/com/example/shualedurinikoloztomaradze/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.shualedurinikoloztomaradze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var answerYes1: CheckBox
+    private lateinit var answerNo1: CheckBox
+    private lateinit var answerYes2: CheckBox
+    private lateinit var answerNo2: CheckBox
+    private lateinit var sendButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
